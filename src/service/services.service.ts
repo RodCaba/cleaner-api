f863e1cc230001131b2858851c13cc7bd863846f
RodCaba <rodser4@gmail.com>
2024-08-02 18:03:25 -0700

    Add overlap logic for services

--- src/service/services.service.ts
+++ src/service/services.service.ts
@@ -11,6 +11,28 @@ export class ServicesService {
     private serviceModel: Model<Service>,
   ) {}
   async create(createServiceDto: CreateServiceDto, userId: string) {
+    const overlapService = await this.serviceModel.findOne({
+      cleanerId: createServiceDto.cleanerId,
+      $or: [
+        {
+          startDateTime: {
+            $gte: createServiceDto.startDateTime,
+            $lte: createServiceDto.endDateTime,
+          },
+        },
+        {
+          endDateTime: {
+            $gte: createServiceDto.startDateTime,
+            $lte: createServiceDto.endDateTime,
+          },
+        },
+      ],
+    });
+
+    if (overlapService) {
+      throw new Error('Service time is overlapped');
+    }
+
     const createdService = new this.serviceModel({
       ...createServiceDto,
       userId,
